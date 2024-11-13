d650b362e4673548843557a34f99ba9c90b9ad7e
Lohan Calot <lohan.calot.etu@univ-lille.fr>
2024-11-13 01:24:14 +0100

    added camera system, room in scene & fixed render issues

---
+++ Assets/Shreddhovens/Scripts/TravellingScenario.cs
@@ -0,0 +1,71 @@
+using Cinemachine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct CameraTransitions
+{
+    public float transitionDelay;
+    public CinemachineVirtualCamera destination;
+}
+
+public class TravellingScenario : MonoBehaviour
+{
+    [SerializeField] private CameraTransitions baseCamera;
+    [SerializeField] private List<CameraTransitions> camerasTransitions = new();
+    [SerializeField] float m_loopDelay = 20f;
+    [SerializeField] bool m_loopMode = false;
+    private int _idxCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetCameras();
+
+        StartCoroutine(StartScenario());
+    }
+
+    private void ResetCameras()
+    {
+        foreach (CameraTransitions tr in camerasTransitions)
+        {
+            tr.destination.enabled = false;
+        }
+        baseCamera.destination.enabled = true;
+        _idxCamera = 0;
+    }
+
+    private IEnumerator StartScenario()
+    {
+        if (camerasTransitions.Count > 0)
+        {
+            while (true)
+            {
+                if (_idxCamera == 0)
+                {
+                    yield return new WaitForSeconds(baseCamera.transitionDelay);
+                }
+                if (_idxCamera >= camerasTransitions.Count)
+                {
+                    if (m_loopMode) ResetCameras();
+                    else yield break;
+                    //StartCoroutine(ResetCamerasCoroutine());
+                }
+                else
+                {
+                    camerasTransitions[_idxCamera].destination.enabled = true;
+                    yield return new WaitForSeconds(camerasTransitions[_idxCamera].transitionDelay);
+                    _idxCamera++;
+                }
+            }
+        }
+    }
+
+    private IEnumerator ResetCamerasCoroutine()
+    {
+        yield return new WaitForSeconds(m_loopDelay);
+
+        ResetCameras();
+    }
+}
